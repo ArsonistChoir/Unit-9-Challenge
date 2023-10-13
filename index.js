097b37787009ed8646e6e9056a22267e2eb44864
ArsonistChoir <haiden2004ellis@outlook.com>
2023-10-12 21:56:15 -0500

    Added similar code from mini project

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = ({ title, description, installation, usage, contribution, test, links, github}) =>
+ `# ${title}
+  ## Description
+  
+  ${description}
+  
+  ## Installation
+  
+  ${installation}
+  
+  ## Usage
+  
+  ${usage}
+  
+  ## Contribution
+  
+  ${contribution}
+  
+  ## Test
+  
+  ${test}
+  
+  ## Links
+  
+  ${links}
+  
+  ## Github
+  
+  ${github}`;
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer
+ .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your chosen title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is you description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your app?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use the app?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How do would other contribute to the project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test the app?'
+    }
+ ])
+  .then((answers) => {
+    console.log(answers);
+    const markdownPageContent = writeToFile(answers);
 
-// Function call to initialize app
-init();
+    fs.writeFile('madeReadMe.md', markdownPageContent, (err) => 
+    err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
+
+
--- pseudo.md
+++ pseudo.md
@@ -1,5 +1,6 @@
 Create readme file
 Add it with title, desription, installation instructions, usage info, contribution guide, and test instructions
+
 Append user input for each of these
 Give user choice for liscense
 Add and Append user github and link
