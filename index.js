a07b33838896e208e2574bfb828f7597d20c467c
ArsonistChoir <haiden2004ellis@outlook.com>
2023-10-12 23:12:20 -0500

    Add table of contents, conditional statement for license, and links and github section

--- index.js
+++ index.js
@@ -2,8 +2,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const writeToFile = ({ title, description, installation, usage, contribution, test, links, github}) =>
- `# ${title}
+const writeToFile = ({ title, description, installation, usage, contribution, test, links, github, license }) => {
+
+    let licenseBadge = "";
+
+    if (license === 'None') {
+        licenseBadge = "";
+    } else if (license === "MIT") {
+        licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "Apache") {
+        licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "Creative Commons") {
+        licenseBadge = " [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+    };
+
+    return `${licenseBadge}
+    
+# ${title}
+
+  ## Table of Content
+
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contribution](#contribution)
+
+  [Test](#test)
+
+  [Links](#links)
+
+  [Github](#github)
+
+  [License](#license)
+
   ## Description
   
   ${description}
@@ -30,48 +64,70 @@ const writeToFile = ({ title, description, installation, usage, contribution, te
   
   ## Github
   
-  ${github}`;
+  ${github}
+  
+  ## License
+  
+  ${license}`
+};
+
 
 inquirer
- .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your chosen title?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is you description?'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your app?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use the app?'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'How do would other contribute to the project?'
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'How do you test the app?'
-    }
- ])
-  .then((answers) => {
-    console.log(answers);
-    const markdownPageContent = writeToFile(answers);
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your chosen title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is you description?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do you install your app?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use the app?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How do would other contribute to the project?'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'How do you test the app?'
+        },
+        {
+            type: 'input',
+            name: 'links',
+            message: 'What the link for your project?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What your github account link?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license would you like?',
+            choices: ['MIT', 'Apache', 'Creative Commons', 'None']
+        }
+    ])
+    .then((answers) => {
+        console.log(answers);
+        const markdownPageContent = writeToFile(answers);
 
-    fs.writeFile('madeReadMe.md', markdownPageContent, (err) => 
-    err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-  });
+        fs.writeFile('madeReadMe.md', markdownPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
 
 
---
+++ madeReadMe.md
@@ -0,0 +1,53 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+# .
+
+  ## Table of Content
+
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contribution](#contribution)
+
+  [Test](#test)
+
+  [Links](#links)
+
+  [Github](#github)
+
+  [License](#license)
+
+  ## Description
+  
+  .
+  
+  ## Installation
+  
+  .
+  
+  ## Usage
+  
+  .
+  
+  ## Contribution
+  
+  .
+  
+  ## Test
+  
+  .
+  
+  ## Links
+  
+  undefined
+  
+  ## Github
+  
+  undefined
+  
+  ## License
+  
+  MIT
